49e9d18431483dd929a05404ebb02c36635ffaf2
mrlutik <yaievgeniy@gmail.com>
2023-05-28 17:05:35 +0200

    Fix bug with pkg naming

--- cmd/autoflowhub/main.go
+++ cmd/autoflowhub/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	tool "github.com/mrlutik/autoflowhub/internal/cli"
-	"github.com/mrlutik/autoflowhub/pkg/accgen/accgencmd"
 	"github.com/mrlutik/autoflowhub/pkg/keygen/keygencmd"
+	"github.com/mrlutik/autoflowhub/pkg/txsgen/txsgencmd"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	cmds := []*cobra.Command{accgencmd.New(), keygencmd.New()}
+	cmds := []*cobra.Command{txsgencmd.New(), keygencmd.New()}
 	cli := tool.NewCLI(cmds)
 	cobra.CheckErr(cli.Execute())
 }

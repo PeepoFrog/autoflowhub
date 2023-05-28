28e3e065f8e576d6bbdf7c8e26a9f7ad193b7cd5
mrlutik <yaievgeniy@gmail.com>
2023-05-28 16:49:26 +0200

    Add autoflowhub main

--- cmd/autoflowhub/main.go
+++ cmd/autoflowhub/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	tool "github.com/mrlutik/autoflowhub/internal/cli"
+	"github.com/mrlutik/autoflowhub/pkg/accgen/accgencmd"
+	"github.com/mrlutik/autoflowhub/pkg/keygen/keygencmd"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	cmds := []*cobra.Command{accgencmd.New(), keygencmd.New()}
+	cli := tool.NewCLI(cmds)
+	cobra.CheckErr(cli.Execute())
+}
